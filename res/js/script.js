a71e04b6c927b3d70f65993a0fdcb23c8666be15
Sochuks <moseschukwumakajang@gmail.com>
2022-11-28 15:04:56 +0100

    - Modified index (Adding animation)   - added javascript rersource to scrollrevealjs.com - Modified script   - created variable sr as an instance of srollreveal set (distance45px, duration2700, reset-true)   - created two objects of sr for home-text and home-img set to appear from left & right respectively.    - created one object of sr for all remaining section of page using class and set to appear from bottom

--- res/js/script.js
+++ res/js/script.js
@@ -16,3 +16,15 @@ window.onscroll = () => {
   menu.classList.remove('bx-x');
   navlist.classList.remove('active')
 };
+
+
+const sr = ScrollReveal({
+  distance : '45px',
+  duration : 2700,
+  reset : true
+})
+
+sr.reveal('.home-text', {delay:350, origin:'left'})
+sr.reveal('.home-img', {delay:350, origin:'right'})
+
+sr.reveal('.sub-services, .about, .portfolio, .services, .cta, .subscribe', {delay:200, origin:'bottom'})
